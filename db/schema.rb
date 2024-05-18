abd432d243fd711163502bc9170f47f38aac2d08
uzuhiro1 <uzuhiro1@gmail.com>
2024-05-18 12:23:15 +0900

    nokogiriをインストール

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_24_024617) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_12_025025) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -68,6 +68,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_24_024617) do
     t.datetime "updated_at", null: false
     t.index ["child_post_id"], name: "index_comments_on_child_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "events", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.date "date"
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "favorites", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
---
+++ spec/factories/events.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :event do
+    date "2024-05-12"
+    title "MyString"
+    description "MyText"
+  end
+end
